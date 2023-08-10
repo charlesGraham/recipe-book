b444600638e1e98bb097e8509a0c961946b1c25f
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-08-10 11:53:43 -0500

    Added recipe model & added structure for recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  public recipes: Recipe[] = [
+    new Recipe("Test", "Test", "https://www.chilipeppermadness.com/wp-content/uploads/2023/01/Buldak-Ramen-SQ-500x500.jpg"),
+  ];
+
+  public contstructor() { }
+
+  public ngOnInit() { }
 
 }
