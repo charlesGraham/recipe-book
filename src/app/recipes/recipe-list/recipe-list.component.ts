54ed4ce95d5f34a327f2d0af6c09f56bb8c41ebc
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-08-11 00:51:35 -0500

    Bound recipe data to recipe-details component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent {
   public recipes: Recipe[] = [
     new Recipe("Test", "Test", "https://www.persnicketyprints.com/wp-content/uploads/2020/09/Jenna-cards1.jpg"),
-    new Recipe("Buldak Ramyun", "Buldak Ramyun is a popular instant noodles!", "https://www.chilipeppermadness.com/wp-content/uploads/2023/01/Buldak-Ramen-SQ-500x500.jpg")
+    new Recipe("Buldak Ramyun", "Buldak Ramyun is a popular instant noodles product!", "https://www.chilipeppermadness.com/wp-content/uploads/2023/01/Buldak-Ramen-SQ-500x500.jpg")
   ];
   @Output() public recipeClicked = new EventEmitter<Recipe>();
 
