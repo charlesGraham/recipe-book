2198e69095dd9f53a43f2870f8d3df1fe254df10
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-08-10 21:09:52 -0500

    Separated recipe-list component into recipe-list & recipe-item components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   public recipes: Recipe[] = [
-    new Recipe("Test", "Test", "https://www.chilipeppermadness.com/wp-content/uploads/2023/01/Buldak-Ramen-SQ-500x500.jpg"),
+    new Recipe("Test", "Test", "https://m.media-amazon.com/images/I/416lI5JIf3L._AC_UF894,1000_QL80_.jpg"),
+    new Recipe("Buldak Ramyun", "Buldak Ramyun is a popular instant noodles!", "https://www.chilipeppermadness.com/wp-content/uploads/2023/01/Buldak-Ramen-SQ-500x500.jpg")
   ];
 
   public contstructor() { }
